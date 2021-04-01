f67125131c7859d51cabd7af22362a3d80fb047a
johanpmeert <johan_meert@skynet.be>
2021-04-01 14:09:13 +0200

    various updates

--- src/com/johanpmeert/JpmBitcoinRpcClient.java
+++ src/com/johanpmeert/JpmBitcoinRpcClient.java
@@ -1,42 +1,37 @@
 package com.johanpmeert;
 
 /**
-
-The goal of this support Class is to be have a series of RPC calls that will work with all of the following coin flavours/nodes:
-- Bitcoin BTC (Bitcoind or bitcoin-QT), version 0.19.1
-- Bitcoin Cash BCH (Bitcoin ABC), version 0.21.4
-- Bitcoin BSV, version 1.0.3
-- Bitcoin Gold BTG, version 0.17.1
-Any differences in the RPC implementation between the nodes will be resolved by the class.
-
-Change the values of the Enum CoinData for access to all your nodes
-
-Supported RPC calls at this time:
-- ValidateBitcoinAddress
-- ValidateBitcoinAddressDetails
-- GetBlockHash
-- GetBlock
-- GetTransactionsFromBlock
-- GetRawTransaction
-- GetBlockCount
-- GetNetworkInfo
-
-**********************************************************
-LICENSE INFORMATION
-
-This software is released under APACHE LICENSE V2.0
-Please read details on http://www.apache.org/licenses/
-***********************************************************
-
-*/
+ * The goal of this support Class is to be have a series of RPC calls that will work with all of the following coin flavours/nodes:
+ * - Bitcoin BTC (Bitcoind or bitcoin-QT), version 0.19.1
+ * - Bitcoin Cash BCH (Bitcoin ABC), version 0.21.4
+ * - Bitcoin BSV, version 1.0.3
+ * - Bitcoin Gold BTG, version 0.17.1
+ * Any differences in the RPC implementation between the nodes will be resolved by the class.
+ * <p>
+ * Change the values of the Enum CoinData for access to all your nodes
+ * <p>
+ * Supported RPC calls at this time:
+ * - ValidateBitcoinAddress
+ * - ValidateBitcoinAddressDetails
+ * - GetBlockHash
+ * - GetBlock
+ * - GetTransactionsFromBlock
+ * - GetRawTransaction
+ * - GetBlockCount
+ * - GetNetworkInfo
+ * <p>
+ * *********************************************************
+ * LICENSE INFORMATION
+ * <p>
+ * This software is released under APACHE LICENSE V2.0
+ * Please read details on http://www.apache.org/licenses/
+ * **********************************************************
+ */
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.math.BigDecimal;
 import java.net.*;
 
@@ -47,7 +42,7 @@ public class JpmBitcoinRpcClient {
      */
     public enum CoinData {
         BTC("BTC", "jswemmelbtc", "ZEHUGANovjdw7jJUuZZr1L2sa", "192.168.2.16", 8332),
-        BCH("BCH", "johan", "4E1tr5lvU", "192.168.2.9", 8332),
+        BCH("BCH", "johan", "g4RP7QAncx4vr", "192.168.2.126", 8332),
         BSV("BSV", "johan", "uFJpoKDJAxFo", "192.168.2.17", 8332),
         BTG("BTG", "johan", "pci8VNgiMoyC", "192.168.2.10", 8332),
         EMPTY("not_a_valid_coin", "", "", "", 0);
@@ -70,11 +65,11 @@ public class JpmBitcoinRpcClient {
     private final String PROTOCOL = "http";
     private HttpURLConnection connection = null;
     private URL rpcUrl;
-    private CoinData typeOfBitcoin;
+    private final CoinData typeOfBitcoin;
 
     /**
      * Creates an instance connection to the node
-     * @param typeofbitcoin
+     * @param typeofbitcoin an instance of the Enum to see which node type you want
      */
     public JpmBitcoinRpcClient(CoinData typeofbitcoin) {
         this.typeOfBitcoin = typeofbitcoin;
@@ -89,6 +84,7 @@ public class JpmBitcoinRpcClient {
         try {
             rpcUrl = new URL(PROTOCOL + "://" + typeOfBitcoin.RPC_ADDRESS + ":" + typeOfBitcoin.RPC_PORT + "/");
         } catch (MalformedURLException e) {
+            System.out.println("Node " + typeOfBitcoin.Label + " is offline");
             e.printStackTrace();
         }
     }
@@ -177,7 +173,7 @@ public class JpmBitcoinRpcClient {
      * @return String
      */
     public String GetBlockHash(int blockNr) {
-        final String POST_PARAMS = "{\"method\":\"getblockhash\",\"params\":["+blockNr+"]}";
+        final String POST_PARAMS = "{\"method\":\"getblockhash\",\"params\":[" + blockNr + "]}";
         try {
             connection = (HttpURLConnection) rpcUrl.openConnection();
             connection.setRequestMethod("POST");
@@ -216,7 +212,7 @@ public class JpmBitcoinRpcClient {
      * @return Block
      */
     public Block GetBlock(String blockHash) {
-        final String POST_PARAMS = "{\"method\":\"getblock\",\"params\":[\""+blockHash+"\",2]}";
+        final String POST_PARAMS = "{\"method\":\"getblock\",\"params\":[\"" + blockHash + "\",2]}";
         try {
             connection = (HttpURLConnection) rpcUrl.openConnection();
             connection.setRequestMethod("POST");
@@ -236,10 +232,10 @@ public class JpmBitcoinRpcClient {
             // There are BSV blocks that return JSON objects > 250Mb and even up into the gigabyte range !
             do {
                 extra = brin.readLine();
-                if (extra!=null) builder.append(extra);
-            } while (extra!=null);
+                if (extra != null) builder.append(extra);
+            } while (extra != null);
             brin.close();
-            String jsonResponse=builder.toString().trim();
+            String jsonResponse = builder.toString().trim();
             // System.out.println(jsonResponse);
             Gson gson = new GsonBuilder().setLenient().create();  // take care of "malformed JSON" error
             Block block = gson.fromJson(jsonResponse, Block.class);
@@ -332,7 +328,7 @@ public class JpmBitcoinRpcClient {
      * @return
      */
     public TransactionsInBlock GetTransactionsFromBlock(String blockHash) {
-        final String POST_PARAMS = "{\"method\":\"getblock\",\"params\":[\""+blockHash+"\",1]}";
+        final String POST_PARAMS = "{\"method\":\"getblock\",\"params\":[\"" + blockHash + "\",1]}";
         try {
             connection = (HttpURLConnection) rpcUrl.openConnection();
             connection.setRequestMethod("POST");
@@ -346,16 +342,13 @@ public class JpmBitcoinRpcClient {
             BufferedReader brin = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             StringBuilder builder = new StringBuilder();
             String extra;
-            // Reading multiple lines below is necessary for the strange behaviour of only the BSV node, otherwise only a part of the block is read
-            // Using BufferedReader should normally do the trick (it does for the other nodes)
-            // Also using StringBuilder append because block 479469 in BSV node overloads the heap using strings
-            // There are BSV blocks that return JSON objects > 245Mb !
+            // Same remarks as with GetBlock are valid here even though this JSON response is much smaller
             do {
                 extra = brin.readLine();
-                if (extra!=null) builder.append(extra);
-            } while (extra!=null);
+                if (extra != null) builder.append(extra);
+            } while (extra != null);
             brin.close();
-            String jsonResponse=builder.toString().trim();
+            String jsonResponse = builder.toString().trim();
             // System.out.println(jsonResponse);
             Gson gson = new GsonBuilder().setLenient().create();  // take care of "malformed JSON" error
             TransactionsInBlock trib = gson.fromJson(jsonResponse, TransactionsInBlock.class);
@@ -400,7 +393,7 @@ public class JpmBitcoinRpcClient {
      * @return
      */
     public SingleTransaction GetRawTransaction(String txId) {
-        final String POST_PARAMS = "{\"method\":\"getrawtransaction\",\"params\":[\""+txId+"\",2]}";
+        final String POST_PARAMS = "{\"method\":\"getrawtransaction\",\"params\":[\"" + txId + "\",2]}";
         try {
             connection = (HttpURLConnection) rpcUrl.openConnection();
             connection.setRequestMethod("POST");
@@ -414,16 +407,13 @@ public class JpmBitcoinRpcClient {
             BufferedReader brin = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             StringBuilder builder = new StringBuilder();
             String extra;
-            // Reading multiple lines below is necessary for the strange behaviour of only the BSV node, otherwise only a part of the block is read
-            // Using BufferedReader should normally do the trick (it does for the other nodes)
-            // Also using StringBuilder append because block 479469 in BSV node overloads the heap using strings
-            // There are BSV blocks that return JSON objects > 245Mb !
+            // Same remarks as with GetBlock are valid here even though this JSON response is much smaller
             do {
                 extra = brin.readLine();
-                if (extra!=null) builder.append(extra);
-            } while (extra!=null);
+                if (extra != null) builder.append(extra);
+            } while (extra != null);
             brin.close();
-            String jsonResponse=builder.toString().trim();
+            String jsonResponse = builder.toString().trim();
             // System.out.println(jsonResponse);
             Gson gson = new GsonBuilder().setLenient().create();  // take care of "malformed JSON" error
             SingleTransaction trib = gson.fromJson(jsonResponse, SingleTransaction.class);
@@ -560,6 +550,66 @@ public class JpmBitcoinRpcClient {
         public String address;
         public int port;
         public int score;
+    }
+
+    /**
+     * Independant method to get the coin balance from my private server through JSON
+     * @param checkAddress coin address string
+     * @param coinData type of coin (BTC, BCH, BSV or BTG) using an enum
+     * @return BigDecimal value of balance
+     */
+    public static BigDecimal GetCoinValueFromJPM(String checkAddress, CoinData coinData) {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        try {
+            URL url = new URL("http://johanpmeert.ddns.net:8100/balance/" + coinData.Label + "=" + checkAddress);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            BufferedReader brin = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String jsonResponse = brin.readLine();
+            brin.close();
+            Gson gson = new GsonBuilder().setLenient().create();  // take care of "malformed JSON" error
+            JPMbalance jpmBalance = gson.fromJson(jsonResponse.trim(), JPMbalance.class);
+            return jpmBalance.balance;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return BigDecimal.ZERO;
+    }
+
+    public class JPMbalance {
+        public BigDecimal balance;
+        public CoinData currency;
+        public int blockheight;
+        public String errortype;
+    }
+
+    public static byte[] hexStringToByteArray(String hex) {
+        hex = hex.length() % 2 != 0 ? "0" + hex : hex;
+        byte[] b = new byte[hex.length() / 2];
+        for (int i = 0; i < b.length; i++) {
+            int index = i * 2;
+            int v = Integer.parseInt(hex.substring(index, index + 2), 16);
+            b[i] = (byte) v;
+        }
+        return b;
+    }
+
+    public static String byteArrayToHexString(byte[] bytes) {
+        final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = HEX_ARRAY[v >>> 4];
+            hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 
 }
